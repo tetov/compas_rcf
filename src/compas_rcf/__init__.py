0b8213c550274c0a74481a57493d024dc78b9095
tetov <anton@tetov.se>
2020-02-11 12:21:21 +0100

    Bump version to 0.1.10

--- src/compas_rcf/__init__.py
+++ src/compas_rcf/__init__.py
@@ -21,7 +21,7 @@ __author__ = ["Anton T Johansson"]
 __copyright__ = "MAS DFAB 1920 students and tutors"
 __license__ = "MIT License"
 __email__ = "anton@tetov.se"
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 
 HERE = os.path.dirname(__file__)
