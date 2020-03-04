560c73128505164d59fc989cbfd766b765d19768
tetov <anton@tetov.se>
2020-03-04 09:25:15 +0100

    Moving abb_runner

--- src/compas_rcf/abb_runner.py
+++ src/compas_rcf/abb_runner.py
@@ -1,6 +1,6 @@
 """Fabrication runner for Rapid Clay Fabrication project for fullscale structure.
 
-Run from command line using :code:`python -m compas_rcf.fabrication.abb_rcf_runner`
+Run from command line using :code:`python -m compas_rcf.abb_rcf_runner`
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -8,11 +8,11 @@ from __future__ import print_function
 
 import json
 import logging as log
+import pathlib
 import sys
 import time
 from datetime import datetime
 from operator import attrgetter
-import pathlib
 
 from compas.geometry import Frame
 from compas.geometry import Point
@@ -24,16 +24,16 @@ from compas_rrc import PrintText
 from compas_rcf import __version__
 from compas_rcf.abb import connection_check
 from compas_rcf.abb import docker_compose_paths
-from compas_rcf.abb import robot_ips
 from compas_rcf.abb import pick_bullet
 from compas_rcf.abb import place_bullet
 from compas_rcf.abb import post_procedure
 from compas_rcf.abb import pre_procedure
+from compas_rcf.abb import robot_ips
 from compas_rcf.docker import compose_up
 from compas_rcf.fabrication.clay_obj import ClayBulletEncoder
 from compas_rcf.fabrication.conf import FABRICATION_CONF as fab_conf
-from compas_rcf.fabrication.conf import interactive_conf_setup
 from compas_rcf.fabrication.conf import Path
+from compas_rcf.fabrication.conf import interactive_conf_setup
 from compas_rcf.utils import ui
 from compas_rcf.utils.json_ import load_bullets
 
@@ -245,8 +245,8 @@ def abb_run():
     ############################################################################
 
     for i, bullet in enumerate(to_place):
-        current_bullet_desc = "Bullet {:03}/{:03} with id {}".format(
-            i + 1, len(to_place), bullet.bullet_id
+        current_bullet_desc = "Bullet {:03}/{:03} with id {}.".format(
+            i, len(to_place) - 1, bullet.bullet_id
         )
 
         abb.send(PrintText(current_bullet_desc))
