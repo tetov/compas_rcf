dd67f44d5d9cf4781517f276962b0694b94f10bb
tetov <anton@tetov.se>
2020-06-29 21:04:29 +0200

    Bumped compas_fab

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def _read(*names, **kwargs):
 long_description = _read("README.md")
 
 requirements = [
-    "compas_fab ~= 0.10.2",
+    "compas_fab ~= 0.11",
     "compas_rrc ~= 0.2.2",
     "questionary ~= 1.5.1",
     "confuse ~= 1.0.0",
@@ -40,7 +40,7 @@ extras_require = {
         "pytest >= 3.2",
         "recommonmark >=0.6",
         "sphinx_compas_theme >= 0.4",
-        "sphinx > =1.6",
+        "sphinx >=1.6",
         "setuptools_scm[toml]",
     ]
 }
