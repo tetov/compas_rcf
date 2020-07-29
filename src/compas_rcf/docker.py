755d25e1f42cd3e3ccb50b233466719d427ca177
tetov <anton@tetov.se>
2020-07-29 10:25:15 +0200

    Working setup. Todo in commit msg
    * Fix connection to ROS from grasshopper
    * Integrate gh file with fab_data format
    * Weird libgl error when using Windows XServer
    * Attribution to GC in GH file.

--- src/compas_rcf/docker.py
+++ src/compas_rcf/docker.py
@@ -58,6 +58,7 @@ def _run(cmd, check_output=False, print_output=True, **kwargs):
 
 def compose_up(
     path,
+    overrides=None,
     force_recreate=False,
     remove_orphans=False,
     ignore_orphans=True,
@@ -69,8 +70,10 @@ def compose_up(
 
     Parameters
     ----------
-    path : :class:`os.PathLike` or :class:`str`
+    path : :class:`os.PathLike`
         Path to compose file.
+    overrides : :obj:`list` of :class:`os.PathLike`, optional
+        Docker compose override files, e.g. ``docker-compose.override.yml``.
     force_recreate : :class:`bool`, optional
         Force recreation of containers specified in ``docker-compose`` file.
         Defaults to ``False``.
@@ -94,9 +97,15 @@ def compose_up(
     run_kwargs.update({"check_output": check_output})
     run_kwargs.update({"print_output": print_output})
 
-    cmd_str = 'docker-compose --file "{}" up --detach'.format(path)
-    cmd = shlex.split(cmd_str)
+    file_args = "--file {} ".format(path)
 
+    for o_path in overrides or []:
+        file_args += "--file {} ".format(o_path)
+
+    cmd_str = "docker-compose {} up --detach".format(file_args)
+    print(cmd_str)
+    cmd = shlex.split(cmd_str, posix="win" not in sys.platform)
+    print(cmd)
     log.debug("Env vars: {}".format(env_vars))
 
     if ignore_orphans:
@@ -113,6 +122,7 @@ def compose_up(
         cmd.append("--remove-orphans")
 
     log.debug("Command to run: {}".format(cmd))
+    print("Command to run: {}".format(cmd))
 
     _run(cmd, **run_kwargs)
 
