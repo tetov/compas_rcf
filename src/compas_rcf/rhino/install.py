812c3f453a506ddb37b1f73965c6d52ffb0c8225
tetov <anton@tetov.se>
2020-09-11 15:18:24 +0200

    Install couchdb for rhino

--- src/compas_rcf/rhino/install.py
+++ src/compas_rcf/rhino/install.py
@@ -3,20 +3,38 @@
 compas_rcf.rhino.install
 ******************************************************************************
 
-Installs ``compas_fab``, ``roslibpy``, ``compas_rrc`` & ``compas_rcf`` for
-Rhino 6.
+Installs ``compas_rcf`` and its dependencies to the Rhino IronPython environment.
 """
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import pkgutil
+
 from compas_rhino import install
 
-PACKAGES = ["compas_fab", "roslibpy", "compas_rcf", "compas_rrc"]
+STD_PKGS = ("compas_fab", "roslibpy", "compas_rcf", "compas_rrc")
 
+OPTIONAL_PKGS = ["couchdb"]
+
+INSTALLED_PKGS = [mod.name for mod in list(pkgutil.iter_modules())]
 
 if __name__ == "__main__":
+    pkgs = set(install.INSTALLABLE_PACKAGES)
 
-    packages = set(install.INSTALLABLE_PACKAGES + PACKAGES)
+    for pkg in STD_PKGS:
+        if pkg not in INSTALLED_PKGS:
+            raise RuntimeError("Required package {} is not installed.".format(pkg))
+        pkgs.add(pkg)
 
-    install.install(version="6.0", packages=packages)
+    for pkg in OPTIONAL_PKGS:
+        if pkg in INSTALLED_PKGS:
+            pkgs.add(pkg)
+        else:
+            print(
+                "Skipping {} since it's not installed in current environment.".format(
+                    pkg
+                )
+            )
+
+    install.install(version="6.0", packages=pkgs)
