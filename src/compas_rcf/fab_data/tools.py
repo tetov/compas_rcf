bf52f1cf5aaa0cab4c39de7cc44bb29260b019db
tetov <anton@tetov.se>
2020-09-17 17:06:38 +0200

    Tools for fabdata manipulation.

--- src/compas_rcf/fab_data/tools.py
+++ src/compas_rcf/fab_data/tools.py
@@ -8,6 +8,7 @@ import json
 from collections import OrderedDict
 
 from compas_rcf.fab_data import ClayBullet
+from compas_rcf.utils import CompasObjEncoder
 
 try:
     from pathlib import Path
@@ -70,26 +71,140 @@ def csv_reports(args):
                     row.append(getattr(bullet, attr, None))
 
 
-def load_bullets(file_path):
+def _load_run_data(file_path):
+    with open(str(file_path), mode="r") as fp:
+        run_data = json.load(fp)
+
+    return run_data
+
+
+def _get_fab_data(path_or_dict):
+    if type(path_or_dict) == dict:
+        dict_ = path_or_dict
+    else:
+        dict_ = _load_run_data(path_or_dict)
+
+    return dict_["fab_data"]
+
+
+def load_fabrication_elements(path_or_dict):
     """Load fabrication data from JSON file.
 
     Parameters
     ----------
-    file_path : :class:`os.PathLike` or :class:`str`
-        JSON file to load data from.
+    path_or_dict : :class:`os.PathLike` or :obj:`dict`
+        Path to JSON representation or dictionary representation of fabrication
+        element.
 
     Returns
     -------
-    :class:`list` of :class:`ClayBullet`
+    :obj:`list` of :class:`ClayBullet`
     """
-    with open(str(file_path), mode="r") as fp:
-        run_data = json.load(fp)
+    fab_data = _get_fab_data(path_or_dict)
 
-    # Accept either run_data file or fab_data file
-    fab_data = run_data.get("fab_data") or run_data
+    return [ClayBullet.from_data(data) for data in fab_data]
 
-    clay_cylinders = []
-    for data in fab_data:
-        clay_cylinders.append(ClayBullet.from_data(data))
 
-    return clay_cylinders
+def load_bullets(path_or_dict):
+    print("load_bullets will be removed in the future.")
+    return load_fabrication_elements(path_or_dict)
+
+
+def get_average_cycle_time(path_or_dict):
+    cylinders = load_fabrication_elements(path_or_dict)
+
+    sum_ = 0
+    count = 0
+    for cylinder in cylinders:
+        if cylinder.cycle_time:
+            count += 1
+            sum_ += cylinder.cycle_time
+
+    return sum_ / count
+
+
+def mark_placed(path, from_=None, to=None):
+    """Mark fabrication elemenets as placed in JSON file.
+
+    Parameters
+    ----------
+    path : :class:`os.PathLike`
+    from_ : :obj:`int`, optional
+        Defines lower bound of element index to mark (inclusive).
+    to : :obj:`int`, optional
+        Defines upper bound of element index to mark (exclusive).
+    """
+    update_fabdata_attrs(path, {"bullet_id": 1}, from_=from_, to=None)
+
+
+def renumber_fab_elements(path, prefix=None):
+    run_data = _load_run_data(path)
+    fab_elems = load_fabrication_elements(run_data)
+
+    for i, fab_elem in enumerate(fab_elems):
+        if prefix:
+            fab_elem.bullet_id = "{}{:03d}".format(prefix, i)
+        else:
+            fab_elem.bullet_id = i
+
+    run_data["fab_data"] = fab_elems
+    with open(path, mode="w") as fp:
+        json.dump(run_data, fp, cls=CompasObjEncoder)
+
+
+def update_fabdata_attrs(
+    path, updated_attrs, from_=None, to=None, reset_ids=False, overwrite=False
+):
+    """Update fabrication element's attributes in JSON file.
+
+    Parameters
+    ----------
+    path : :class:`os.PathLike`
+    updated_attrs : :obj:`dict`
+        Dictionary of attribute names and new values.
+    from_ : :obj:`int`, optional
+        Defines lower bound of element index to mark (inclusive).
+    to : :obj:`int`, optional
+        Defines upper bound of element index to mark (exclusive).
+    reset_ids : :obj:`bool`, optional
+        If true will change the `bullet_id` attribute to be the same as the
+        element's index in list. Defaults to ``False``.
+    overwrite : :obj:`bool`, optional
+        Overwrite attributes. Defaults to ``False``.
+    """
+    run_data = _load_run_data(path)
+    cylinders = load_fabrication_elements(run_data)
+
+    if not from_:
+        from_ = 0
+    if not to:
+        to = len(cylinders)
+
+    for i, cylinder in enumerate(cylinders):
+        modified = False
+        if reset_ids:
+            print("Changing id from {} to {}".format(cylinder.bullet_id, i))
+            cylinder.bullet_id = i
+
+        if from_ <= i < to:
+            for key, value in updated_attrs.items():
+                if getattr(cylinder, key, None) is None or overwrite:
+                    setattr(cylinder, key, value)
+                    modified |= True  # a fun OR gate
+
+        if modified:
+            print(
+                "Cylinder with index {} and id {} updated.".format(
+                    i, cylinder.bullet_id
+                )
+            )
+        else:
+            print(
+                "Cylinder with index {} and id {} not updated.".format(
+                    i, cylinder.bullet_id
+                )
+            )
+
+    run_data["fab_data"] = cylinders
+    with open(path, mode="w") as fp:
+        json.dump(run_data, fp, cls=CompasObjEncoder)
