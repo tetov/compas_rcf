a62a284ef7c318d59b6224781503e1ad9b9d1d46
tetov <anton@tetov.se>
2020-10-31 19:00:51 +0100

    Trajectories split into two list like objects

--- src/rapid_clay_formations_fab/fab_data/fabrication_element.py
+++ src/rapid_clay_formations_fab/fab_data/fabrication_element.py
@@ -8,8 +8,8 @@ import math
 import compas.datastructures
 import compas.geometry as cg
 
+from rapid_clay_formations_fab.robots import MinimalTrajectories
 from rapid_clay_formations_fab.robots import MinimalTrajectory
-from rapid_clay_formations_fab.robots import two_levels_reversed
 
 try:
     import Rhino.Geometry as rg
@@ -174,7 +174,7 @@ class FabricationElement(object):
         -------
         :obj:`float`
         """
-        return self.density * self.volume * 1e-6
+        return self.attrs.get("density", 0) * self.volume * 1e-6
 
     # Construct geometrical representations of object using :any:`compas.geometry`.
     ###############################################################################
@@ -400,18 +400,12 @@ class PlaceElement(FabricationElement):
         data.update(
             {
                 "compression_ratio": self.compression_ratio,
-                "travel_trajectories": [
-                    t.to_data() for t in self.travel_trajectories or []
-                ],
-                "return_travel_trajectories": [
-                    t.to_data() for t in self.return_travel_trajectories_ or []
-                ],
-                "place_trajectories": [
-                    t.to_data() for t in self.place_trajectories_ or []
-                ],
-                "return_place_trajectories": [
-                    t.to_data() for t in self.return_place_trajectories_ or []
-                ],
+                "travel_trajectories": self.travel_trajectories.to_data() or None,
+                "return_travel_trajectories": self.return_travel_trajectories.to_data()
+                or None,
+                "place_trajectories": self.place_trajectories.to_data() or None,
+                "return_place_trajectories": self.return_place_trajectories.to_data()
+                or None,
             }
         )
 
@@ -419,23 +413,26 @@ class PlaceElement(FabricationElement):
 
     @data.setter
     def data(self, data):
-        def trajectories_from_data(traj_data_list):
-            if traj_data_list:
-                return [MinimalTrajectory.from_data(d) for d in traj_data_list]
-
         # This is needed to use the setter on parent class
         # https://stackoverflow.com/a/37663266
         super(PlaceElement, self.__class__).data.fset(self, data)
 
         self.compression_ratio = data["compression_ratio"]
-        self.travel_trajectories = trajectories_from_data(data["travel_trajectories"])
-        self.return_travel_trajectories = trajectories_from_data(
-            data["return_travel_trajectories"]
+        self.travel_trajectories = MinimalTrajectories.from_data(
+            data["travel_trajectories"]
         )
-        self.place_trajectories = trajectories_from_data(data["place_trajectories"])
-        self.return_place_trajectories = trajectories_from_data(
-            data["return_place_trajectories"]
-        )
+        if data.get("return_travel_trajectories"):
+            self.return_travel_trajectories = MinimalTrajectories.from_data(
+                data["return_travel_trajectories"]
+            )
+        if data.get("place_trajectories"):
+            self.place_trajectories = MinimalTrajectories.from_data(
+                data["place_trajectories"]
+            )
+        if data.get("return_place_trajectories"):
+            self.return_place_trajectories = MinimalTrajectories.from_data(
+                data["return_place_trajectories"]
+            )
 
     # Trajectories setup
     ######################
@@ -443,8 +440,9 @@ class PlaceElement(FabricationElement):
     @property
     def return_travel_trajectories(self):
         """:class:`rapid_clay_formations_fab.robots.MinimalTrajectory` : Either specific trajectories or reversed travel trajectories."""  # noqa: E501
-        return self.return_travel_trajectories_ or two_levels_reversed(
-            self.travel_trajectories
+        return (
+            self.return_travel_trajectories_
+            or self.travel_trajectories.reversed_recursively()
         )
 
     @return_travel_trajectories.setter
@@ -454,12 +452,14 @@ class PlaceElement(FabricationElement):
     @property
     def place_trajectories(self):
         """:class:`rapid_clay_formations_fab.robots.MinimalTrajectory` : Either specific trajectories or frame trajectories derived from location."""  # noqa: E501
-        return self.place_trajectories_ or [
-            MinimalTrajectory(
-                [self.get_egress_frame(), self.get_uncompressed_top_frame()]
-            ),
-            MinimalTrajectory([self.get_compressed_top_frame()]),
-        ]
+        return self.place_trajectories_ or MinimalTrajectories(
+            [
+                MinimalTrajectory(
+                    [self.get_egress_frame(), self.get_uncompressed_top_frame()]
+                ),
+                MinimalTrajectory([self.get_compressed_top_frame()]),
+            ]
+        )
 
     @place_trajectories.setter
     def place_trajectories(self, trajectories):
@@ -468,8 +468,9 @@ class PlaceElement(FabricationElement):
     @property
     def return_place_trajectories(self):
         """:class:`rapid_clay_formations_fab.robots.MinimalTrajectory` : Either specific trajectories or reversed place trajectories."""  # noqa: E501
-        return self.return_place_trajectories_ or two_levels_reversed(
-            self.place_trajectories
+        return (
+            self.return_place_trajectories_
+            or self.place_trajectories.reversed_recursively()
         )
 
     @return_place_trajectories.setter
---
+++ src/rapid_clay_formations_fab/robots/trajectories.py
@@ -0,0 +1,231 @@
+"""A minimal trajectory class to mix frame sequences and joint trajectories.
+
+Module name in reference to :mod:`class_fab.robots`.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from compas.geometry import Frame
+from compas_fab.robots import Configuration
+from compas_fab.robots import JointTrajectoryPoint
+from compas_fab.robots import to_degrees
+from compas_rrc import RobotJoints
+
+try:
+    from collections.abc import MutableSequence
+except ImportError:
+    from collections import MutableSequence
+
+
+class _ListLike(MutableSequence):
+    """Class to use as parent for listlike classes."""
+
+    def __getitem__(self, index):
+        return self.list_[index]
+
+    def __setitem__(self, index, item):
+        self.list_[index] = item
+
+    def __delitem__(self, index):
+        del self.list_[index]
+
+    def __len__(self):
+        return len(self.list_)
+
+    def insert(self, index, item):
+        """Insert element at specified index."""
+        self.list_.insert(index, item)
+
+
+class MinimalTrajectories(_ListLike):
+    """List of :class:`MinimalTrajectory` objects.
+
+    This class inherits from :obj:`MutableSequence` which means that it behaves
+    like a :obj:`list` exposing methods such as ``index``, ``__iter__`` ``__len__``.
+
+    Parameters
+    ----------
+    trajectories : :obj:`list` of :class:`MinimalTrajectory`
+        Trajectory points.
+    """
+
+    def __init__(self, trajectories):
+        self.trajectories = list(trajectories)
+
+    def __repr__(self):
+        return "MinimalTrajectories({})".format(self.trajectories)
+
+    @property
+    def trajectories(self):
+        """:obj:`list` : List of trajectories."""
+        return self.list_
+
+    @trajectories.setter
+    def trajectories(self, trajectories):
+        self.list_ = trajectories
+
+    @property
+    def data(self):
+        """Get a :obj:`dict` representation of :class:`MinimalTrajectories`."""
+        return {"trajectories": [t.to_data() for t in self.trajectories]}
+
+    @data.setter
+    def data(self, data):
+        self.trajectories = [
+            MinimalTrajectory.from_data(t) for t in data["trajectories"]
+        ]
+
+    def reverse_recursively(self):
+        """Reverse list and the lists elements."""
+        self.reverse()
+        for traj in self:
+            traj.reverse()
+
+    def copy(self):
+        """Get an independent copy of object."""
+        cls = type(self)
+        return cls([o.copy() for o in self])
+
+    def reversed_recursively(self):
+        """Get an independent, recursively reversed copy of object."""
+        copy = self.copy()
+        copy.reverse_recursively()
+        return copy
+
+    def to_data(self):
+        """Get a :obj:`dict` representation of :class:`MinimalTrajectories`."""
+        return self.data
+
+    @classmethod
+    def from_data(cls, data):
+        """Construct a :class:`MinimalTrajectories` from a :obj:`dict` representation.
+
+        Parameters
+        ----------
+        data : :obj:`dict`
+            The data dictionary.
+
+        Returns
+        -------
+        :class:`MinimalTrajectories`
+        """
+        obj = cls([])
+        obj.data = data
+        return obj
+
+
+class MinimalTrajectory(_ListLike):
+    """Trajectory defined either as joint positions or frames.
+
+    This class inherits from UserList which means that it behaves like a
+    :obj:`list` implementing methods such as ``index``, ``__iter__`` and
+    reveresed.
+
+    Parameters
+    ----------
+    points : :obj:`list`
+        Trajectory points.
+    """
+
+    JOINT_TRAJECTORY = 0
+    FRAME_TRAJECTORY = 1
+
+    def __init__(self, points):
+        self.list_ = list(points)
+
+    def __repr__(self):
+        return "MinimalTrajectory({})".format(self.points)
+
+    @property
+    def points(self):
+        """:obj:`list` : List of trajectory points."""
+        return self.list_
+
+    @points.setter
+    def points(self, points):
+        self.list_ = points
+
+    @property
+    def data(self):
+        """Get a :obj:`dict` representation of :class:`MinimalTrajectory`."""
+        return {"points": [pt.to_data() for pt in self.points]}
+
+    @data.setter
+    def data(self, data):
+        if data["points"][0].get("xaxis"):  # check if data is of frame.data
+            self.points = [Frame.from_data(pt) for pt in data["points"]]
+        # check if first elem is JointTrajectoryPoint dict
+        elif data["points"][0].get("types"):
+            self.points = [Configuration.from_data(pt) for pt in data["points"]]
+        else:
+            raise NotImplementedError(
+                "Object type not supported: {}".format(type(data[0]))
+            )
+
+    @property
+    def robot_joints_points(self):
+        """:obj:`list` of :class:`compas_rrc.RobotJoints` : Trajectory as list of ``RobotJoints``."""  # noqa: E501
+        return [RobotJoints(*to_degrees(pt)) for pt in self.points]
+
+    @property
+    def trajectory_type(self):
+        """:obj:`type` : Return the type of elements in the trajectory."""
+        self._raise_if_mixed_types()
+        if isinstance(self[0], JointTrajectoryPoint):
+            return self.JOINT_TRAJECTORY
+        if isinstance(self[0], Frame):
+            return self.FRAME_TRAJECTORY
+
+        raise NotImplementedError("Trajectory not recognized: {}".format(self))
+
+    def _raise_if_mixed_types(self):
+        """Raise error if the list contains mixed types of objects."""
+        types = (type(pt) for pt in self)
+
+        if len(set(types)) != 1:
+            raise RuntimeError(
+                "Trajectory contains more than one type of objects: {}".format(types)
+            )
+
+    def copy(self):
+        """Get an independent copy of object."""
+        cls = type(self)
+        return cls([o.copy() for o in self])
+
+    def reversed(self):
+        """Get an independent, reversed copy of object."""
+        copy = self.copy()
+        copy.reverse()
+
+        return copy
+
+    @classmethod
+    def from_joint_trajectory(cls, joint_trajectory):
+        """Construct a instance from a :class:`compas_fab.robots.JointTrajectory`."""
+        conf_list = [
+            Configuration.from_revolute_values(pt.values)
+            for pt in joint_trajectory.points
+        ]
+        return cls(conf_list)
+
+    def to_data(self):
+        """Get :obj:`dict` representation of :class:`MinimalTrajectory`."""
+        return self.data
+
+    @classmethod
+    def from_data(cls, data):
+        """Construct a :class:`MinimalTrajectory` from a dictionary representation.
+
+        Parameters
+        ----------
+        data : :obj:`dict`
+            The data dictionary.
+
+        Returns
+        -------
+        :class:`MinimalTrajectory`
+        """
+        obj = cls([])
+        obj.data = data
+        return obj
